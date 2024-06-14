7a49df0449224c2fffcc052e2c09eb94dff67b1a
jacques00077 <134284505+jacques00077@users.noreply.github.com>
2024-06-14 01:00:16 +0000

    Update 2-hbnb.py

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -2,9 +2,9 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
-import uuid
 from flask import Flask, render_template, url_for
 from models import storage
+import uuid;
 
 # flask setup
 app = Flask(__name__)
@@ -23,7 +23,7 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/2-hbnb/')
+@app.route('/2-hbnb')
 def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
@@ -34,14 +34,29 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    cache_id = (str(uuid.uuid4()))
     return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
                            states=states,
                            amens=amens,
                            places=places,
-                           users=users,
-                           cache_id=cache_id)
-
+                           users=users)
+@app.route('/')
+def hbnb_filters1(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users)
 
 if __name__ == "__main__":
     """
